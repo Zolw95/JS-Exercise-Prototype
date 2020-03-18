778315cf22ca7df59115c236934ebfc523e56eef
unknown <miloszz87@gmail.com>
2020-03-17 23:22:15 -0400

    completed homework and stretches

--- index.js
+++ index.js
@@ -39,9 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+};
 
-}
+Person.prototype.eat = function(edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+};
+
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
+
+
+
 
 /*
   TASK 2
@@ -57,9 +77,31 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
 }
+
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance) {
+  let gas = distance / this.milesPerGallon;
+  if (this.tank >= gas) {
+    this.odometer += distance;
+    this.tank -= gas; 
+  } else {
+    let newDistance = this.milesPerGallon * this.tank;
+    this.odometer += newDistance;
+    this.tank = 0;
+  }
+  if (this.tank == 0) {
+    return `I ran out of fuel at ${this.odometer}`;
+  }
+};
 
 /*
   TASK 3
@@ -68,9 +110,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+};
 
-}
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.poop = function () {
+  return 'Change my pampers';
+};
+
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`;
+};
+
+Baby.prototype.toString = function () {
+  return `${this.name}, ${this.age} and my favorite toy is ${this.favoriteToy}`;
+};
+
+let johnnie = new Baby('Pola', 8, 'crocodile');
 
 /* 
   TASK 4
